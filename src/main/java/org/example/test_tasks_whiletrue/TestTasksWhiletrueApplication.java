aa7babfe3cae707b782fd6ed999e251f86d46bab
AntonenkoTymofii <antonenkotimofii@gmail.com>
2025-03-24 15:31:01 +0200

    create project

---
+++ src/main/java/org/example/test_tasks_whiletrue/TestTasksWhiletrueApplication.java
@@ -0,0 +1,13 @@
+package org.example.test_tasks_whiletrue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTasksWhiletrueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTasksWhiletrueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/test_tasks_whiletrue/TestTasksWhiletrueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.test_tasks_whiletrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTasksWhiletrueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
